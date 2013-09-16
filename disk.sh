ed8ae3bc403713bd0b8a74d2b08f661b63eaa696
l1905 <853992401@qq.com>
2013-09-16 08:07:44 +0800

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #author:litong
-#memory pra convert json
+#disk pra convert json
 #time:2013/9/13
 iostat -d |awk -vS=2 -vE=6 'NR==3{for(n=S;n<=E;n++)a[n]="\""$n"\":"}NR==4{printf "{"a[S]$S;for(n=S+1;n<=E;n++)printf(","a[n]$n);print "}"}'
