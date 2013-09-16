c27419a59c148b6eac76a59c55123b5ddc0fa48c
l1905 <853992401@qq.com>
2013-09-16 08:06:51 +0800

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #author:litong
 #cpu pra convert json
+#time:2013/9/13
 iostat -c |awk '{if(NR==4) print "{""\"%user\":"$1",","\"%nice\":"$2",","\"%system\":"$3",","\"%iowait\":"$4",","\"%steal\":"$5",","\"%idle\":"$6"}"}'
